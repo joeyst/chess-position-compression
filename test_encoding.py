984353bf3249bce2983cc23a39f43106d667144a
joeyst <joeystenbeck@gmail.com>
2023-06-30 17:53:15 -0700

    Add test_encoding.py script and description.

---
+++ test_encoding.py
@@ -0,0 +1,12 @@
+
+"""
+Used to test the various encoding schemes for their correctness, average size, and speed of encoding, decoding, and both. 
+Encoding schemes: 
+- FEN 
+- Huffman 
+  - Huffman w/o optimizations 
+  - Huffman w/ symmetry optimization 
+  - Huffman w/ default square optimization 
+  - Huffman w/ blank rank/file optimization 
+  - Huffman w/ piece-centric optimization 
+"""
